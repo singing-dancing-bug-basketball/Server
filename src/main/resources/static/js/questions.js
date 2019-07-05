779794d07a390ea2f93636a3d0ee788995e78b22
1160300527 <15683609851@163.com>
2019-07-05 10:19:31 +0800

    题库翻页功能前端完成

--- src/main/resources/static/js/questions.js
+++ src/main/resources/static/js/questions.js
@@ -1,2 +1,41 @@
-var current_page=[[${page}]]
-var total_page=[[${total_page}]]
+$(function(){
+   //var total_page=[[${total_page}]];
+   //var page=[[${page}]];
+   var total_page=10;
+   var page=5;
+   var begin;
+   var end;
+   $("#before").attr("href",1);
+   $("#after").attr("href",total_page);
+   if(total_page<=5) {
+       begin=1;
+       end=total_page;
+   }
+   else{
+    if(page<=3){
+        begin=1;
+        end=5;
+    }
+    else if(page>=total_page-2){
+        begin=total_page-4;
+        end=total_page;
+    }
+    else{
+        begin=page-2;
+        end=page+2;
+    }
+   }
+   for (var i = begin; i <= end; i++) {
+    var href = $("<a></a>").text(i);
+    href.attr("id", "page_" + i);
+    var li = $("<li></li>").html(href);
+    if (i == page) {
+       li.attr("class", "active");
+    }
+    else{
+       href.attr("href", i);
+    }
+    $("#last").before(li);
+ }
+});
+
